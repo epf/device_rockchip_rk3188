c7244d17c057b7db05e4e15b08b1e3575be3ff35
Mike Wilson <mwilson@acesappraisal.com>
2015-03-29 16:27:39 -0400

    Fix some preinstalls
    Start working on Root - needs help here.
    
    Signed-off-by: Mike Wilson <mwilson@acesappraisal.com>

--- preinstall/preinstall.mk
+++ preinstall/preinstall.mk
@@ -1,7 +1,20 @@
 PRODUCT_PACKAGES += SuperUser
 PRODUCT_PACKAGES += RkApkinstaller
 PRODUCT_PACKAGES += ABenchMark
-#PRODUCT_PACKAGES += NovaLauncher
+PRODUCT_PACKAGES += NovaLauncher
 PRODUCT_PACKAGES += ESFileExplorer
 PRODUCT_PACKAGES += WifiAnalyzer
 PRODUCT_PACKAGES += ViPER4Android_FX_A4.x
+PRODUCT_PACKAGES += recovery
+PRODUCT_PACKAGES += ViPER4AndroidPresets
+PRODUCT_PACKAGES += RebootMenu
+
+PRODUCT_COPY_FILES += \
+	device/rockchip/rk3188/preinstall/recovery.img:system/media/install/recovery.img \
+	device/rockchip/rk3188/preinstall/ViPER4AndroidPresets.zip:system/media/install/ViPER4AndroidPresets.zip \
+        device/rockchip/rk3188/preinstall/bootanimation.zip:system/media/bootanimation.zip \
+        device/rockchip/rk3188/preinstall/default_wallpaper.jpg:system/media/rkfactory/default_wallpaper.jpg\
+	device/rockchip/rk3188/preinstall/busybox:system/xbin/busybox \
+	device/rockchip/rk3188/preinstall/su:system/xbin/su \
+
+
